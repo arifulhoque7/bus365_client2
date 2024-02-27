02618c9f2c0c15a09ce63dfcf1dd86c99735cf66
Farshee <sfromio1694@gmail.com>
2024-02-27 11:59:07 +0600

    mpesa auto submit and check validation

--- public/js/mpesa.js
+++ public/js/mpesa.js
@@ -16,15 +16,27 @@ $(document).ready(function () {
 $("#mpesa-pay").click(function () {
     var mobile = $('#login_mobile').val();
     var amount = $('#grandtotal').val();
-    var $env = $(this),
-    $eloaderTarget = $env.parent();
-    bdtaskIlmCommonJs.elemLoader.show($eloaderTarget, 20, false);
+    var first_name = $('#first_name').val();
+    var last_name = $('#last_name').val();
+    var id_type = $('#id_type').find(":selected").val();
+    var id_number = $('#id_number').val();
+   
 
     if(mobile==''){
         alert('Mpesa Registered Mobile Number is Required');
+    }else if(first_name==''){
+            alert('First Name is Required');
+    }else if(last_name==''){
+            alert('Last Name is Required');
+    }else if(id_type==''){
+            alert('ID Type is Required');
+    }else if(id_number==''){
+            alert('ID Number is Required');       
     }else{
         $('#login_mobile').prop('readonly', true);
-
+        var $env = $(this),
+        $eloaderTarget = $env.parent();
+        bdtaskIlmCommonJs.elemLoader.show($eloaderTarget, 20, false);
         var baseurl = $('#baseurl').val();
         var url = baseurl + '/modules/api/v1/paymethods/mpesa/mpesa_pay';
 
@@ -32,8 +44,8 @@ $("#mpesa-pay").click(function () {
             method: "POST",
             url: url,
             data: { 
-                phone: 254708374149, 
-                amount: 1
+                phone: mobile, 
+                amount: amount
             },
             success: function (data) {
                  alert(data.message);
@@ -69,6 +81,7 @@ function validation(checkout_request_id,transection_id){
                     $('#mpesaMsg').text('Payment Successfully Done by User');
                     $('#submit-booking').show();
                     $('#mpesa-pay').hide();
+                    $('#createbooking').submit();
                 }else{
                     $('#mpesaMsg').text(data.message?data.message:data.message.errorMessage);
                 }
